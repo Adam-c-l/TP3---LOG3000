65205b333db7625101951ead5c25d7106dbcb7a7
Alexander Ciaciek <ciaciek.polska@gmail.com>
2026-02-27 16:56:22 -0500

    feat/ added tests for calculate method

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,14 @@
 from app import app, calculate
+
+def test_calculate_positive_left_and_right():
+    assert calculate("1+2") == 3.0
+
+def test_calculate_positive_left_and_right():
+    assert calculate("1*2") == 2.0
+
+def test_calculate_positive_left_and_right():
+    assert calculate("1/2") == 0.5
+
+def test_calculate_positive_left_and_right():
+    assert calculate("1-2") == -1.0
+
